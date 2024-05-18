b4cfd88a4dfe558a175e8ac0dfb3ef1190e5f552
James Naurot <naurotj@gamail.com>
2024-05-18 05:18:36 -0400

    clearing out garbage

--- sb_ideaforce/s6.sql
+++
@@ -1,3 +0,0 @@
-select author, count(author) as NumSuggestions from sbf_suggestion S group by author having count(S.author) > 1;
-select author, count(author) as NumSuggestions from sbf_suggestion S group by author order by count(S.author) desc;
-select author, count(author) as NumSuggestions from sbf_comment S group by author order by count(S.author) desc;
--- testBlob.py
+++
@@ -1,3 +0,0 @@
-from textblob import TextBlob
-text = "donald trump is the worst"
-print(text,": ", TextBlob(text).sentiment)
